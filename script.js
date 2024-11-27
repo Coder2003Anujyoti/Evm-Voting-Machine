138d5dbd179d6ce81b156175119eb20805fd52b2
Anujyoti De <anujyotide@gmail.com>
2024-11-27 17:53:16 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,166 @@
+var show=document.getElementById('show');
+var screen=document.getElementById('screen');
+var container=document.getElementById('container');
+var b1=document.getElementById('b1');
+var b2=document.getElementById('b2');
+var b3=document.getElementById('b3');
+var b4=document.getElementById('b4');
+var b5=document.getElementById('b5');
+
+
+function aoc()
+{ let c1=document.getElementById('c1');
+let bjp=document.getElementById('bjp');
+c1.style.backgroundColor="green";
+  b1.disabled=true;
+  b2.disabled=true;
+  b3.disabled=true;
+  b4.disabled=true;
+  b5.disabled=true;
+  function ro()
+  {
+ container.style.display="none";
+  screen.style.display="block";
+  show.style.display="block";
+ screen.innerHTML="<img src='Bjp.png' id='rao'>";
+   
+  }
+  setTimeout(ro,2000);
+  function bo()
+  { container.style.display="block";
+    show.style.display="none";
+    c1.style.backgroundColor="white";
+    b1.disabled=false;
+  b2.disabled=false;
+  b3.disabled=false;
+  b4.disabled=false;
+  b5.disabled=false;
+  }
+  setTimeout(bo,5000);
+  
+  
+}
+  function boc()
+{ let c2=document.getElementById('c2');
+let bjp=document.getElementById('bjp');
+c2.style.backgroundColor="green";
+  b1.disabled=true;
+  b2.disabled=true;
+  b3.disabled=true;
+  b4.disabled=true;
+  b5.disabled=true;
+  function ro()
+  {
+ container.style.display="none";
+  screen.style.display="block";
+  show.style.display="block";
+ screen.innerHTML="<img src='Cong.png' id='rao'>";
+  }
+  setTimeout(ro,2000);
+  function bo()
+  { container.style.display="block";
+    show.style.display="none";
+    c2.style.backgroundColor="white";
+    b1.disabled=false;
+  b2.disabled=false;
+  b3.disabled=false;
+  b4.disabled=false;
+  b5.disabled=false;
+  }
+  setTimeout(bo,5000);
+  
+  
+}
+function coc()
+{ let c3=document.getElementById('c3');
+let bjp=document.getElementById('bjp');
+c3.style.backgroundColor="green";
+  b1.disabled=true;
+  b2.disabled=true;
+  b3.disabled=true;
+  b4.disabled=true;
+  b5.disabled=true;
+  function ro()
+  {
+ container.style.display="none";
+  screen.style.display="block";
+  show.style.display="block";
+ screen.innerHTML="<img src='Cpim.png' id='rao'>";
+  }
+  setTimeout(ro,2000);
+  function bo()
+  { container.style.display="block";
+    show.style.display="none";
+    c3.style.backgroundColor="white";
+    b1.disabled=false;
+  b2.disabled=false;
+  b3.disabled=false;
+  b4.disabled=false;
+  b5.disabled=false;
+  }
+  setTimeout(bo,5000);
+  
+  
+}
+ function doc()
+{ let c4=document.getElementById('c4');
+let bjp=document.getElementById('bjp');
+c4.style.backgroundColor="green";
+  b1.disabled=true;
+  b2.disabled=true;
+  b3.disabled=true;
+  b4.disabled=true;
+  b5.disabled=true;
+  function ro()
+  {
+ container.style.display="none";
+  screen.style.display="block";
+  show.style.display="block";
+ screen.innerHTML="<img src='Tmc.png' id='rao'>";
+  }
+  setTimeout(ro,2000);
+  function bo()
+  { container.style.display="block";
+    show.style.display="none";
+    c4.style.backgroundColor="white";
+    b1.disabled=false;
+  b2.disabled=false;
+  b3.disabled=false;
+  b4.disabled=false;
+  b5.disabled=false; 
+  }
+  setTimeout(bo,5000);
+  
+  
+}
+function eoc()
+{ let c5=document.getElementById('c5');
+let bjp=document.getElementById('bjp');
+c5.style.backgroundColor="green";
+  b1.disabled=true;
+  b2.disabled=true;
+  b3.disabled=true;
+  b4.disabled=true;
+  b5.disabled=true;
+  function ro()
+  {
+ container.style.display="none";
+  screen.style.display="block";
+  show.style.display="block";
+ screen.innerHTML="<img src='Aap.webp' id='rao'>";
+  }
+  setTimeout(ro,2000);
+  function bo()
+  { container.style.display="block";
+    show.style.display="none";
+    c5.style.backgroundColor="white";
+    b1.disabled=false;
+  b2.disabled=false;
+  b3.disabled=false;
+  b4.disabled=false;
+  b5.disabled=false;  
+  }
+  setTimeout(bo,5000);
+  
+  
+}
